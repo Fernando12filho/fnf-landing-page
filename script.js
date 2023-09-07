ee6cffdc0ff6aa85d87f682b6d36ef38b43c98b0
Fernando Melo <fgmelofilho@icloud.com>
2023-09-07 13:41:27 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+menu = document.getElementById("dropdownMenu");
+var window = window.innerWidth;
+
+function openMenu(){
+    menu.style.display = "flex";
+}
+
+function closeMenu(){
+    menu.style.display = "none"
+}
+
+
+
+
